ca9f3533f7928ca84bbae9355488c830fa60c393
DaffaDev <142648571+Dappzzz-Dev@users.noreply.github.com>
2026-02-22 22:31:49 +0700

    overview

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -63,6 +63,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../app/dashboard/overview/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/dashboard/overview">> = Specific
+  const handler = {} as typeof import("../../app/dashboard/overview/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../app/dashboard/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/dashboard">> = Specific
